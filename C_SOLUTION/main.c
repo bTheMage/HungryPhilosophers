8c98a96387b9f2f9fe3f8fd99b138004b3255afb
bTheMage <108481203+bTheMage@users.noreply.github.com>
2022-11-05 20:28:03 -0300

    .

--- C_SOLUTION/main.c
+++ C_SOLUTION/main.c
@@ -14,22 +14,18 @@ const int n = 3;
 
 // MACROS
 // Use this macro if shit hit the fan.
-#define PANIC(MSG, ...) {                           \
-    (void) fprintf (stderr, MSG "\n", __VA_ARGS__); \
-    exit(EXIT_FAILURE);                             \
-}
+#define PANIC(MSG) {fprintf (stderr, MSG); exit(EXIT_FAILURE);}
 
-// Just a variadic free macro. Use it to free a lot of stuf at once.
-#define FREE(X, ...) {                                                      \
-    void* __to_be_freed[] = {X, __VA_ARGS__};                               \
-    int __n_to_be_freed = sizeof(__to_be_freed) / sizeof(__to_be_freed[0]); \
-    for (int __i = 0; i < __n_to_be_freed; i++)                             \
-        free (__to_be_freed);                                               \
-}
-
+// This macro iterates throgh a fixed size static array and free all its pointers.
+#define FREEEACH(ARRAY) {                                   \
+    int __num_of_pnt = sizeof(ARRAY) / sizeof(ARRAY[0]);    \
+    for (int __i = 0; __i < __num_of_pnt; __i++)            \
+        free (ARRAY[__i]);                                  \
+}                                                           
 
 // GLOBAL VARIABLES
 int* tids;
+int* chopsticks_state;
 pthread_t* philosophers;
 pthread_mutex_t* chopsticks;
 pthread_mutex_t bowl;
@@ -38,20 +34,46 @@ pthread_mutex_t bowl;
 
 // MAIN FUNCTION
 int main (int argc, char *argv[]) {
+    printf("Hello, World!\n");
+
+    // VARIABLES
+    int mainflag = 0;
+
     // ALLOCATING MEMORY
     philosophers = (pthread_t*) malloc (n * sizeof(pthread_t));
-    if (NULL == philosophers) // ERROR HANDLING
-        PANIC("FAILED TO ALLOCATE MEMORY FOR THE PHILOSOPHERS PTHREAD VECTOR.\n");
-
+    tids = (int*) calloc (n, sizeof(int));
+    chopsticks_state = (int*) malloc ((n) * sizeof(int));
     chopsticks = (pthread_mutex_t*) malloc ((n + 1) * sizeof(pthread_mutex_t));
-    if (NULL == chopsticks) { // ERROR HANDLING
-        free(philosophers); 
-        PANIC("FAILED TO ALLOCATE MEMORY FOR THE PHILOSOPHERS PTHREAD VECTOR.");
+
+    void* pointers[] = {philosophers, tids, chopsticks_state, chopsticks};
+
+    // ERROR HANDLING
+    if (!(philosophers && tids && chopsticks_state && chopsticks)) {
+        FREEEACH(pointers);
+        PANIC("FAILED TO ALLOCATE MEMORY.");
     }
 
     // INITIALIZING GLOBAL VARIABLES
-    pthread_mutex_init ()
+    for (int i = 0; i <= n; i++)
+        if (pthread_mutex_init (&chopsticks[i], NULL)) {
+            FREEEACH(pointers);
+            PANIC("FAILED TO INITIATE A MUTEX.");
+        }
 
+    
+
+
+
+    // DESTROYING THE MUTEXES
+    for (int i = 0; i <= n; i++)
+        if (pthread_mutex_destroy (&chopsticks[i])) {
+            FREEEACH(pointers);
+            PANIC("FAILED TO INITIATE A MUTEX.");
+        }
 
     // FREEING THE MEMORY
+    FREEEACH(pointers);
+
+
+    printf("Good Bye, World!\n");
 }
